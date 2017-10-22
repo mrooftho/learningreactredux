b8a5b320013334b9e18f95d69cd7c1306b216f29
PROD\mrooftho <michel.roofthooft@staff.telenet.be>
2017-10-22 17:19:41 +0200

    added React added ReactDom added first Welcome app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import upper from './upper';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-const app = document.getElementById('app');
+class Welcome extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {name : 'MiRo'};
+    }
 
-app.innerHTML = 'Hello ' + upper('miro');
+    handleChange(e){
+        this.setState({name: e.target.value});
+    }
+
+    render(){
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Welcome</h1>
+                <p>Hello {this.state.name}</p>
+                <input onChange={this.handleChange} defaultValue={this.state.name}/>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<Welcome/>,document.getElementById('app'));
